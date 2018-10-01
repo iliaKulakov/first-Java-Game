022f20879d101c24401d7d0c6981add800d36bd8
ilia <38052676+iliaKulakov@users.noreply.github.com>
2018-10-01 23:28:06 +0300

    reformat code

--- src/main/java/io/github/maventest/model/board/Ocean.java
+++ src/main/java/io/github/maventest/model/board/Ocean.java
@@ -29,7 +29,6 @@ public class Ocean implements Board {
         }
     }
 
-
     @Override
     public void print() {
         for (int i = 0; i < SIZE_H; i++) {
@@ -51,7 +50,6 @@ public class Ocean implements Board {
         CellSet<Cell> shipCells = new CellSet<>();
 
 //      TODO: Сформировать координаты отностительно занятости и позции
-        // this.generateCell(shipLength, shipCells, horizontal)
         this.generateCell(shipLength, shipCells, horizontal);
 
         shipCells.forEach(cell -> {
@@ -66,24 +64,12 @@ public class Ocean implements Board {
             boats.put(shipCells, ship);
             return;
         }
-        /*} else{
-
-          boats.containsKey(shipCells)
-
-
-
-        }*/
-        //System.out.println(boats.containsKey(),boats.containsValue());
-
 
     }
 
-
     private void generateCell(int shipLength, CellSet<Cell> cells, boolean horizontal) {
-        // private CellSet<Cell>  generateCell(int shipLength, CellSet<Cell> cells, boolean horizontal) {
         int randomW = randomGenerator.nextInt(SIZE_W);
         int randomH = randomGenerator.nextInt(SIZE_H);
-
 
         for (int i = 0; i < shipLength; i++) {
             Cell cell = new Cell(randomW, randomH);
@@ -101,19 +87,10 @@ public class Ocean implements Board {
                     randomW++;
                 }
 //                TODO: Проверка на горизонтальность
-                //        int x = cellsArray[0].getCoordinateX();
-//        int y = cellsArray[0].getCoordinateY();
-//
-//        if (horizontal) {
-//            cellsArray[1] = new Cell(x, y + 1);
-//        } else {
-//            cellsArray[1] = new Cell(x + 1, y);
-//        }
 
                 cells.add(cell);
             }
         }
-        //return cells;
     }
 
     private boolean isOccupied(CellSet<Cell> cells) {
@@ -127,18 +104,6 @@ public class Ocean implements Board {
         return status;
     }
 
-
-//    public boolean isOccupied(Ship Boat) {
-//        if (this.units.contains(Boat)) {
-//            System.out.println("Cells with ships");
-//            return true;
-//
-//        } else {
-//            System.out.println("Cells without ships");
-//            return false;
-//
-//        }
-//    }
 
     public void printUnits() {
         //units.forEach(System.out::print);
